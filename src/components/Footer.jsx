b9d680869abe625778833e93d23a51212bc9074b
antoniobiasotti <abamorim36@gmail.com>
2024-04-19 14:35:10 -0300

    style: footer componet

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 export function Footer() {
     return(
         <footer className={styles.Footer}>
-            <p>Made with React by <a href='https://github.com/antoniobiasotti' target='_blank'>Antonio</a></p>
+            <p>Made with <a href="https://react.dev" target="_blank">React</a> by <a href="https://github.com/antoniobiasotti" target="_blank">Antonio</a></p>
         </footer>
     )
 }
